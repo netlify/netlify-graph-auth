4ff6f7cd0bab7824dad5c0c963699fca6814b504
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-07-22 11:37:09 -0700

    unknown

--- src/helpers.ts
+++ src/helpers.ts
@@ -5,22 +5,17 @@ type error = {
   };
 };
 
-type results = {
-  errors?: error[];
-  graphQLErrors?: error[];
-};
-
-type MissingAuthService = {graphQLField: string};
+export type MissingAuthService = {graphQLField: string};
 
 export function findMissingAuthServices(
-  results: results | error[],
+  results: Record<string, unknown> | error[],
 ): MissingAuthService[] {
   /* Detect and normalize between:
     1. The full graphql result
     2. The `result.errors` of a graphql result
     3. Apollo's GraphQL error structure
      */
-  let errors: error[] | undefined;
+  let errors: unknown[] | undefined;
 
   if (Array.isArray(results)) {
     errors = results;
@@ -28,27 +23,42 @@ export function findMissingAuthServices(
     results != null &&
     ('errors' in results || 'graphQLErrors' in results)
   ) {
-    errors =
-      // Full GraphQL result
-      results.errors ||
+    if (Array.isArray(results.errors)) {
+      errors = results.errors; // Full GraphQL result
+    } else if (Array.isArray(results.graphQLErrors)) {
       // Apollo error
-      results.graphQLErrors;
+      errors = results.graphQLErrors;
+    }
   }
 
   if (!Array.isArray(errors)) {
     return [];
   }
 
-  const missingServiceErrors = errors.filter(
-    (error) => error?.extensions?.type === 'auth/missing-auth',
-  );
+  const missingServiceErrors = errors.filter((error) => {
+    const isObject = typeof error == 'object' && error != null;
+
+    if (isObject && error.hasOwnProperty('extensions')) {
+      // @ts-expect-error
+      error.extensions && error.extensions?.type === 'auth/missing-auth';
+    }
+
+    return false;
+  });
 
   const missingServices = missingServiceErrors
     .map((error) => {
-      const field = error?.extensions?.graphQLField;
+      if (
+        typeof error == 'object' &&
+        error != null &&
+        error.hasOwnProperty('extensions')
+      ) {
+        // @ts-expect-error
+        const field = error.extensions?.graphQLField;
 
-      if (field != null) {
-        return {graphQLField: field};
+        if (field != null) {
+          return {graphQLField: field};
+        }
       }
     })
     .filter(Boolean) as Array<MissingAuthService>;
