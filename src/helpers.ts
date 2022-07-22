752cb8e8d0bbd4c61ca519ef134c189bb8c9cd80
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-07-21 20:04:51 -0700

    fix types

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,30 +2,35 @@ type error = {
   extensions?: {
     type: string;
     graphQLField?: string;
-  }
-}
+  };
+};
 
 type results = {
   errors?: error[];
   graphQLErrors?: error[];
 };
 
-export function findMissingAuthServices(results: results): string[] {
+export function findMissingAuthServices(results: results | error[]): string[] {
   /* Detect and normalize between:
     1. The full graphql result
     2. The `result.errors` of a graphql result
     3. Apollo's GraphQL error structure
      */
-  let errors =
-    results &&
-    // Full GraphQL result
-    (results.errors ||
-      // Apollo error
-      results.graphQLErrors ||
-      // Possibly result.errors
-      results);
+  let errors: error[] | undefined;
 
-  // If errors aren't an array, bail
+  if (Array.isArray(results)) {
+    errors = results;
+  } else if (
+    results != null &&
+    ('errors' in results || 'graphQLErrors' in results)
+  ) {
+    errors =
+      // Full GraphQL result
+      results.errors ||
+      // Apollo error
+      results.graphQLErrors;
+  }
+
   if (!Array.isArray(errors)) {
     return [];
   }
