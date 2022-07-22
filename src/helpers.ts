92d75c9044828b35503380df14dbe03a5b5c994d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-07-22 08:37:11 -0700

    Type findMissingAuthServices (#6)
    * Type findMissingAuthServices
    
    * fix types
    
    * return `{graphQLField: 'field'}`

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,19 +1,40 @@
-export function findMissingAuthServices(results: any) {
+type error = {
+  extensions?: {
+    type: string;
+    graphQLField?: string;
+  };
+};
+
+type results = {
+  errors?: error[];
+  graphQLErrors?: error[];
+};
+
+type MissingAuthService = {graphQLField: string};
+
+export function findMissingAuthServices(
+  results: results | error[],
+): MissingAuthService[] {
   /* Detect and normalize between:
     1. The full graphql result
     2. The `result.errors` of a graphql result
     3. Apollo's GraphQL error structure
      */
-  let errors =
-    results &&
-    // Full GraphQL result
-    (results.errors ||
-      // Apollo error
-      results.graphQLErrors ||
-      // Possibly result.errors
-      results);
+  let errors: error[] | undefined;
 
-  // If errors aren't an array, bail
+  if (Array.isArray(results)) {
+    errors = results;
+  } else if (
+    results != null &&
+    ('errors' in results || 'graphQLErrors' in results)
+  ) {
+    errors =
+      // Full GraphQL result
+      results.errors ||
+      // Apollo error
+      results.graphQLErrors;
+  }
+
   if (!Array.isArray(errors)) {
     return [];
   }
@@ -23,8 +44,14 @@ export function findMissingAuthServices(results: any) {
   );
 
   const missingServices = missingServiceErrors
-    .map((error) => error.extensions.graphQLField)
-    .filter(Boolean);
+    .map((error) => {
+      const field = error?.extensions?.graphQLField;
+
+      if (field != null) {
+        return {graphQLField: field};
+      }
+    })
+    .filter(Boolean) as Array<MissingAuthService>;
 
   return missingServices;
 }
